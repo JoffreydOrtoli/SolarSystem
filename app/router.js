44986e08728b028b4cddf338893342a46f992040
JoffreydOrtoli <joffrey.dortoli@hotmail.fr>
2022-01-23 01:05:29 +0100

    les 3 input liés j'adore

--- app/router.js
+++ app/router.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const planets = require('./data/planets');
-
+const countrys = require('./data/countrys');
 const router = express.Router();
 
 router.get("/", (req, res) => {
@@ -8,25 +8,10 @@ router.get("/", (req, res) => {
 })
 
 router.get('/solarsystem', (req, res) => {
-    if (req.query.search) {
-        const filterPlanets = [];
-        const loweredQuery = req.query.search.toLowerCase();
-        for (const planet of planets) {
-            if(planet.name.includes(loweredQuery)) {
-                filterPlanets.push(planet);
-            }
-        }
-        res.render('solarsystem', {
-            planets: filterPlanets,
-        });
-    }
-    else {
         res.render('solarsystem', {
             planets: planets,
-        })
-    }
-})
-
+        });
+});
 router.get('/planete/:name', (req, res, next) => {
     const askedPlanets = req.params.name;
 
@@ -47,8 +32,29 @@ router.get('/planete/:name', (req, res, next) => {
     }
 });
 
+router.get('/terre/countrys', (req, res) => {
+    const filterArray = [];
+    if (req.query.searchCountry || req.query.searchCapital || req.query.searchContinent) {
+        const loweredQueryCountry = req.query.searchCountry.toLowerCase();
+        const loweredQueryCapital = req.query.searchCapital.toLowerCase();
+        const loweredQueryContinent = req.query.searchContinent.toLowerCase();
+        for (const country of countrys) {
+            if(country.name.toLowerCase().includes(loweredQueryCountry) && country.capital.toLowerCase().includes(loweredQueryCapital) && country.continent.toLowerCase().includes(loweredQueryContinent)) {
+                filterArray.push(country);
+            };
+        };
+        res.render('countrys', {
+            countrys: filterArray,
+        });
+    }
+    else {
+        res.render("countrys", {
+            countrys: countrys,
+        });
+    } 
+});
+
 router.use(function (req, res) {
     res.status(404).render('notfound');
 });
-
   module.exports = router;
